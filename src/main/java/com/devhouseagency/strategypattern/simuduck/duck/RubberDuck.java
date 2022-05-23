e3b6afe31415de47daabf7fd309a497080ba866e
officialtalent <talentsqwabe@gmail.com>
2022-05-23 18:37:29 +0200

    Deleted dead code.

--- src/main/java/com/devhouseagency/strategypattern/simuduck/duck/RubberDuck.java
+++ src/main/java/com/devhouseagency/strategypattern/simuduck/duck/RubberDuck.java
@@ -1,7 +1,5 @@
 package com.devhouseagency.strategypattern.simuduck.duck;
 
-import com.devhouseagency.strategypattern.simuduck.duck.Duck;
-
 public class RubberDuck extends Duck {
     @Override
     public void display() {
