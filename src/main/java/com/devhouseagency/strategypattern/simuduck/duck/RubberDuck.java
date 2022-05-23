60fbe39457b15a3ad304b5ce7fdf4367c255201c
officialtalent <talentsqwabe@gmail.com>
2022-05-23 17:59:33 +0200

    Removed quack() method in RubberDuck class.

--- src/main/java/com/devhouseagency/strategypattern/simuduck/duck/RubberDuck.java
+++ src/main/java/com/devhouseagency/strategypattern/simuduck/duck/RubberDuck.java
@@ -3,15 +3,6 @@ package com.devhouseagency.strategypattern.simuduck.duck;
 import com.devhouseagency.strategypattern.simuduck.duck.Duck;
 
 public class RubberDuck extends Duck {
-    /*
-    * Rubber ducks don't quack, so quack() is
-    * overridden to "Squeak".
-    * */
-    @Override
-    public void quack() {
-        System.out.println("Squeak");
-    }
-
     @Override
     public void display() {
         System.out.println("Rubber Duck");
