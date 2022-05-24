d0c6e9c616e880b6ac0ba0a3c3cc572605cd2f30
officialtalent <talentsqwabe@gmail.com>
2022-05-24 12:56:31 +0200

    Added main method in AdventureGame class.

--- src/main/java/com/devhouseagency/strategypattern/adventuregame/AdventureGame.java
+++ src/main/java/com/devhouseagency/strategypattern/adventuregame/AdventureGame.java
@@ -1,4 +1,14 @@
 package com.devhouseagency.strategypattern.adventuregame;
 
+import com.devhouseagency.strategypattern.adventuregame.character.*;
+import com.devhouseagency.strategypattern.adventuregame.character.Character;
+
 public class AdventureGame {
+    public static void main(String[] args) {
+        Character[] characters = {new Queen(), new King(), new Troll(), new Knight()};
+
+        for (Character character : characters) {
+            character.fight();
+        }
+    }
 }
